c48f32236b224067eea7d1e112d08d7aab77a22a
Lee Danilek <lee@convex.dev>
2023-01-26 18:35:02 -0800

    bug where unique returns null now

--- convex/readSecret.ts
+++ convex/readSecret.ts
@@ -3,9 +3,12 @@ import { getValidWhisper } from '../expiration';
 
 export default query(async ({ db }, whisperName: string, accessKey: string): Promise<string> => {
   const whisperDoc = await getValidWhisper(db, whisperName, false);
-  await db
+  const accessDoc = await db
     .query('accesses')
     .withIndex('by_name_and_key', q => q.eq('name', whisperName).eq('accessKey', accessKey))
     .unique();
+  if (!accessDoc) {
+    throw new Error("accessKey invalid");
+  }
   return whisperDoc.encryptedSecret;
 })
