bbea04b6a74142c10020d73c3ac3060a98e9366f
Lee Danilek <lee@convex.dev>
2023-10-13 17:12:23 -0400

    production

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 if [[ $VERCEL_ENV == "preview"  ]] ; then 
 	export NEXT_PUBLIC_CONVEX_URL=$(npx convex preview $VERCEL_GIT_COMMIT_REF) && npm run build
-elif [[ $VERCEL_ENV == "prod" ]]; then  
+elif [[ $VERCEL_ENV == "production" ]]; then  
   npm run build && npx convex deploy
 else
 	# Command for dev if relevant
