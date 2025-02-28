50180899243cfaa08ea8194f35d779fd15dcd4da
Lee Danilek <lee@convex.dev>
2025-02-28 11:58:13 -0500

    upgrade to convex 1.19

--- convex/readExpiration.ts
+++ convex/readExpiration.ts
@@ -1,17 +1,20 @@
 import { query } from './_generated/server'
 import { readExpiration } from '../expiration';
 import { timingSafeEqual } from './security';
-import { ConvexError } from 'convex/values';
+import { ConvexError, v } from 'convex/values';
 
 // Returns description of when the whisper expires, and a timestamp of when to next check.
 // Input currentTime invalidates the cache.
-export default query(async ({ db }, {name, creatorKey, currentTime}: {name: string, creatorKey: string, currentTime: number}): Promise<[string, number | null, boolean]> => {
-  const whisperDoc = await db
-    .query('whispers')
-    .withIndex('by_name', q => q.eq('name', name))
-    .unique();
-  if (!timingSafeEqual(whisperDoc!.creatorKey, creatorKey)) {
-    throw new ConvexError('invalid creator key');
-  }
-  return readExpiration(db, name);
+export default query({
+  args: {name: v.string(), creatorKey: v.string(), currentTime: v.number()},
+  handler: async ({ db }, {name, creatorKey, currentTime}) => {
+    const whisperDoc = await db
+      .query('whispers')
+      .withIndex('by_name', q => q.eq('name', name))
+      .unique();
+    if (!timingSafeEqual(whisperDoc!.creatorKey, creatorKey)) {
+      throw new ConvexError('invalid creator key');
+    }
+    return readExpiration(db, name);
+  },
 });
